757ea87fb5b13c8dc9fe22b72e65e76437e9c482
isosafrasaurus <143103585+isosafrasaurus@users.noreply.github.com>
2024-08-19 21:18:16 -0700

    first draft time dependency

--- modules/coupler_kdtree.py
+++ modules/coupler_kdtree.py
@@ -12,7 +12,7 @@ class RadiusFunction(UserExpression):
     A user-defined expression to compute the radius at a given point based on the nearest control point in a graph.
 
     Args:
-        G (networkx.Graph): The graph containing control points with radius information.
+        G (graphnics.FenicsGraph): The graph containing control points with radius information.
         mf (dolfin.MeshFunction): A mesh function associated with the graph.
         kdtree (scipy.spatial.cKDTree): A k-d tree for efficient nearest neighbor search in the graph.
         **kwargs: Additional keyword arguments to be passed to the UserExpression constructor.
@@ -83,7 +83,7 @@ def run_perfusion_univ(G, directory_path, del_Omega=3.0, perf3=9.6e-2, perf1=1.4
     Runs a perfusion simulation on a given graph and saves the results for a universal boundary condition.
 
     Args:
-        G (networkx.Graph): The graph representing the network.
+        G (graphnics.FenicsGraph): The graph representing the network.
         directory_path (str): The directory where the results will be saved
         del_Omega (float, optional): Boundary condition value for 3D pressure. Defaults to 3.0
         perf3 (float, optional): 3D perfusion coefficient
@@ -196,7 +196,7 @@ def run_perfusion(G, directory_path, del_Omega=3.0, perf3=9.6e-2, perf1=1.45e4, 
     Runs a perfusion simulation on a given graph and saves the results.
 
     Args:
-        G (networkx.Graph): The graph representing the network.
+        G (graphnics.FenicsGraph): The graph representing the network.
         directory_path (str): The directory where the results will be saved
         del_Omega (float, optional): Boundary condition value for 3D pressure. Defaults to 3.0
         perf3 (float, optional): 3D perfusion coefficient
@@ -276,7 +276,7 @@ def run_perfusion(G, directory_path, del_Omega=3.0, perf3=9.6e-2, perf1=1.45e4, 
     D_perimeter = 2 * np.pi * radius_function
     
     # Blocks
-    a00 = perf3 * inner(grad(u3), grad(v3)) * dx + kappa * inner(u3_avg, v3_avg) * D_perimeter * dxLambda
+    a00 = perf3 * inner(grad(u3), grad(v3)) * dxOmega + kappa * inner(u3_avg, v3_avg) * D_perimeter * dxLambda
     a01 = -kappa * inner(u1, v3_avg) * D_perimeter * dxLambda
     a10 = -kappa * inner(u3_avg, v1) * D_perimeter * dxLambda
     a11 = perf1 * inner(grad(u1), grad(v1)) * D_area * dxLambda + kappa * inner(u1, v1) * D_perimeter * dxLambda - gamma * inner(u1, v1) * dsLambda(1)
@@ -300,6 +300,134 @@ def run_perfusion(G, directory_path, del_Omega=3.0, perf3=9.6e-2, perf1=1.45e4, 
     uh3d, uh1d = wh
     uh3d.rename("3D Pressure", "3D Pressure Distribution")
     uh1d.rename("1D Pressure", "1D Pressure Distribution")
+
+    # Create output directory if it doesn't exist and save
+    os.makedirs(directory_path, exist_ok=True)
+    output_file_1d = os.path.join(directory_path, "pressure1d.vtk")
+    output_file_3d = os.path.join(directory_path, "pressure3d.pvd")
+    save_mesh_as_vtk(Lambda, output_file_1d, radius_function, uh1d=uh1d)
+    File(output_file_3d) << uh3d
+    
+    return output_file_1d, output_file_3d, uh1d, uh3d
+
+def run_perfusion_univ_time(G, directory_path, del_Omega=3.0, perf3=9.6e-2, perf1=1.45e4, kappa=3.09e-5, gamma=1.0, P_infty=1.0e3, dt=1e-2, num_steps=10):        
+    """
+    Runs a time-dependent perfusion simulation using the backward Euler method.
+
+    Args:
+        G (graphnics.FenicsGraph): The graph representing the network.
+        directory_path (str): The directory where the results will be saved
+        del_Omega (float, optional): Boundary condition value for 3D pressure. Defaults to 3.0
+        perf3 (float, optional): 3D perfusion coefficient
+        perf1 (float, optional): 1D perfusion coefficient
+        kappa (float, optional): Coupling coefficient
+        gamma (float, optional): Boundary condition coefficient for 1D pressure
+        P_infty (float, optional): Boundary condition value for 1D pressure
+        dt (float, optional): Time step size
+        num_steps (int, optional): Number of time steps to run the simulation
+
+    Returns:
+        tuple: A tuple containing:
+            - output_file_1d (str): The path to the saved 1D pressure VTK file
+            - output_file_3d (str): The path to the saved 3D pressure PVD file
+            - uh1d (dolfin.Function): The computed 1D pressure function
+            - uh3d (dolfin.Function): The computed 3D pressure function
+    """    
+    # Create \Lambda
+    G.make_mesh()
+    Lambda, mf = G.get_mesh()
+    
+    # Reference copy of \Lambda
+    H = G.copy()
+    
+    # Create \Omega
+    Omega = UnitCubeMesh(16, 16, 16)
+
+    # Translate all \Lambda points to positive, same in H
+    pos = nx.get_node_attributes(G, "pos")
+    node_coords = np.asarray(list(pos.values()))
+    xmin, ymin, zmin = np.min(node_coords, axis = 0)
+    d = Lambda.coordinates()
+    d[:, :] += [-xmin, -ymin, -zmin]
+    for node in H.nodes:
+        H.nodes[node]['pos'] = np.array(H.nodes[node]['pos']) + [-xmin, -ymin, -zmin]
+    
+    # \Lambda k-d tree
+    kdtree = cKDTree(np.array(list(nx.get_node_attributes(H, 'pos').values())))
+
+    # Fit \Omega around \Lambda
+    c = Omega.coordinates()
+    xl, yl, zl = (np.max(node_coords, axis=0)-np.min(node_coords, axis=0))
+    c[:,:] *= [xl+3, yl+3, zl]
+    
+    def boundary_Omega(x, on_boundary):
+        return on_boundary and not near(x[2], 0) and not near(x[2], zl)
+        
+    # Constants
+    kappa = Constant(kappa)
+    gamma = Constant(gamma)
+    P_infty = Constant(P_infty)
+    del_Omega = Constant(del_Omega)
+    dt = Constant(dt)
+    perf3 = Constant(perf3)
+    perf1 = Constant(perf1)
+
+    # Function spaces
+    V3 = FunctionSpace(Omega, "CG", 1)
+    V1 = FunctionSpace(Lambda, "CG", 1)
+    W = [V3, V1]
+    u3, u1 = list(map(TrialFunction, W))
+    v3, v1 = list(map(TestFunction, W))
+    
+    radius_function = RadiusFunction(G, mf, kdtree)
+    cylinder = Circle(radius=radius_function, degree=5)
+    u3_avg = Average(u3, Lambda, cylinder)
+    v3_avg = Average(v3, Lambda, cylinder)
+
+    # Dirac measures
+    dxOmega = Measure("dx", domain=Omega)
+    dxLambda = Measure("dx", domain=Lambda)
+    dsLambda = Measure("ds", domain=Lambda)
+    
+    # Define D_area and D_perimeter
+    D_area = np.pi * radius_function ** 2
+    D_perimeter = 2 * np.pi * radius_function
+    
+    # Initialize previous solutions u3^n and u1^n
+    u3_n = Function(V3)
+    u1_n = Function(V1)
+
+    # Time-stepping
+    for n in range(num_steps):
+        # Variational forms
+        a00 = (1/dt) * inner(u3, v3) * dxOmega + perf3 * inner(grad(u3), grad(v3)) * dxOmega + kappa * inner(u3_avg, v3_avg) * D_perimeter * dxLambda
+        a01 = -kappa * inner(u1, v3_avg) * D_perimeter * dxLambda
+        a10 = -kappa * inner(u3_avg, v1) * D_perimeter * dxLambda
+        a11 = (1/dt) * inner(u1, v1) * dxLambda + perf1 * inner(grad(u1), grad(v1)) * D_area * dxLambda + kappa * inner(u1, v1) * D_perimeter * dxLambda
+        
+        # Right-hand side
+        L0 = (1/dt) * inner(u3_n, v3) * dxOmega + inner(Constant(0), v3_avg) * dxOmega
+        L1 = (1/dt) * inner(u1_n, v1) * dxLambda + inner(Constant(0), v1) * dxLambda
+        
+        a = [[a00, a01], [a10, a11]]
+        L = [L0, L1]
+    
+        W_bcs = [[DirichletBC(V3, del_Omega, boundary_Omega)], []]
+
+        A, b = map(ii_assemble, (a, L))
+        A, b = apply_bc(A, b, W_bcs)
+        A, b = map(ii_convert, (A, b))
+
+        wh = ii_Function(W)
+        solver = LUSolver(A, "mumps")
+        solver.solve(wh.vector(), b)
+        uh3d, uh1d = wh
+        uh3d.rename("3D Pressure", "3D Pressure Distribution")
+        uh1d.rename("1D Pressure", "1D Pressure Distribution")
+        
+        # Update previous solution
+        u3_n.assign(uh3d)
+        u1_n.assign(uh1d)
 
     # Create output directory if it doesn't exist and save
     os.makedirs(directory_path, exist_ok=True)
